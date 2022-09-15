c570949ffffa3e38434271e23db61da33d0f4f03
AlanTXM <alanteixeirademoraes850@gmail.com>
2022-09-15 16:42:10 -0300

    Login

---
+++ Login/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { identifierName } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { identity } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Login';
+
+
+
+
+}
+
