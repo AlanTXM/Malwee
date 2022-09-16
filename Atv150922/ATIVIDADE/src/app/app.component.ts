3b60935e5d472f9462a372d8672617a334b8f4ad
AlanTXM <alanteixeirademoraes850@gmail.com>
2022-09-16 17:01:26 -0300

    Nao terminado

--- Atv150922/ATIVIDADE/src/app/app.component.ts
+++ Atv150922/ATIVIDADE/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  
 })
 export class AppComponent {
   title = 'ATIVIDADE';
+
+  nContato: String | undefined;
+  tContato: String | undefined;
+
+  salvarDados(nome: String, telefone: String){
+    this.nContato = nome;
+    this.tContato = telefone;
+  }
 }
--- Atv150922/ATIVIDADE/src/app/app.module.ts
+++ Atv150922/ATIVIDADE/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, FormBuilder, Validators} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgIfNgForComponent } from './ng-if-ng-for/ng-if-ng-for.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgIfNgForComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
